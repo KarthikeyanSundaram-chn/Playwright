b9280693a3158fda68904e9460db3071b3503193
KarthikeyanSundaram-chn <Karthikeyn.Sundaram@yahoo.com>
2026-04-14 19:01:22 +0530

    Fifth

--- tests/playwright/Alert.spec.ts
+++ tests/playwright/Alert.spec.ts
@@ -1,7 +1,6 @@
-import test from "@playwright/test";
-test("Alert handling", async({page}) => {
+import { test } from "@playwright/test";
 
-test.only("dialog", async({page})=>{
+test("Alert handling", async({page}) => {
 
     page.once('dialog', alertType => {
         const type = alertType.type();
@@ -11,14 +10,22 @@ test.only("dialog", async({page})=>{
         alertType.accept();
     })
     
+    await page.goto("https://www.leafground.com/alert.xhtml");
+    await page.locator("button:has-text('Show')").first().click()
+    await page.waitForTimeout(5000);
+})
 
-} )
+test("dialog", async({page})=>{
 
-
-await page.goto("https://www.leafground.com/alert.xhtml");
-await page.locator("button:has-text('Show')").first().click()
-await page.waitForTimeout(5000);
-
-
-}
-)
+    page.once('dialog', alertType => {
+        const type = alertType.type();
+        console.log(type);
+        console.log(alertType.message());
+        console.log(`Dialog message: ${alertType.message()}`);
+        alertType.accept();
+    })
+    
+    await page.goto("https://www.leafground.com/alert.xhtml");
+    await page.locator("button:has-text('Show')").first().click()
+    await page.waitForTimeout(5000);
+})
